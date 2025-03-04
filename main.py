d17fc50adfd64ddfad8e64a2fb51654a53b0dee9
plima94 <pauloalima1994@gmail.com>
2025-03-04 18:33:11 +0000

    update readme with a description

--- main.py
+++ main.py
@@ -1,63 +1,41 @@
-path_to_file = "books/frankenstein.txt"
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
-   
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        
-    
-    word_count = len(file_contents.split())
-    print(word_count)
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-def character_counter(path_to_file):
-    
-    with open(path_to_file, 'r') as f:
-        file_contents = f.read()
-
-    
-    text = file_contents.split()
-    
-    
-    characters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    char_count = {}
-
-    
-    for char in characters:
-        char_count[char] = 0
-
-    
-    for word in text:
-        word = word.lower() 
-        for char in word:
-            if char in characters:  
-                char_count[char] += 1
-    
-    char_count[' '] += file_contents.count(' ')
-
-    return char_count
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
-def print_report():
-    dict = character_counter(path_to_file)
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-    list = [{key: value} for key, value in dict.items()]
-    
-    for dictionary in list:
-        for key, value in dictionary.items():
-            if key.isalpha():  
-                print(f"The '{key}' character was found '{value}' times")
-    
+    print("============= END ===============")
 
-    
 
-    
-
-             
-
-        
-        
-        
 main()
-character_counter(path_to_file)
-print_report()
